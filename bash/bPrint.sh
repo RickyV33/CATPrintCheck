ac5e6ec157ebc238b5116dd2ce2d45605ac76f6a
RickyV33 <3d.RickyV@gmail.com>
2015-02-12 13:18:18 -0800

    Remove useless echo from bash script

--- bash/bPrint.sh
+++ bash/bPrint.sh
@@ -8,9 +8,9 @@ printers=(eb325bw1 eb325bw2 eb325clr1 eb423bw1 eb423clr1 fab5517clr1 \
           fab5517bw1 fab5517bw2 fab6001bw1 fab6019bw1 fab8201bw1 fabc8802bw1) 
 
 
-#Code notes: Incorperate the time stamp at the beginning of the file in bash
+#Code notes: Incorporate the time stamp at the beginning of the file in bash
+#Split up eb and fab printers and give user options
 for printer in "${printers[@]}"
 do
-    echo 'ls -la'
-    #echo "lpr -P $printer ../printPage.py"
+    echo "lpr -P $printer ../printPage.py"
 done
